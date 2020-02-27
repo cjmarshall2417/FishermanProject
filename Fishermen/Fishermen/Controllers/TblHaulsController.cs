b4e7c9e169a94efb909951583a5cd45614394bce
CJ Marshall <cjmarshall2417@gmail.com>
2020-02-27 11:12:26 -0800

    Added group by functionality to custom query, changed BestMonthInArea to return 0 if no entries for a given month

--- Fishermen/Fishermen/Controllers/TblHaulsController.cs
+++ Fishermen/Fishermen/Controllers/TblHaulsController.cs
@@ -16,6 +16,7 @@ namespace Fishermen.Controllers
 {
     public class TblHaulsController : Controller
     {
+        public string[] months = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
         private readonly PhishermenContext fishHauls;
 
         public TblHaulsController(PhishermenContext context)
@@ -25,8 +26,8 @@ namespace Fishermen.Controllers
 
         [HttpGet]
         [Route("api/TopTenHaulsByDate")]
-        //returns the best ten hauls on a given date
-        public IActionResult TopTenHaulsByDate(int month, int year, bool ascendingSort)
+        //returns the best (or worst) hauls on a given date
+        public IActionResult TopTenHaulsByDate(int month, int year, bool ascendingSort, int rows = 10)
         {
             var url = GetUrlPath();
 
@@ -42,12 +43,12 @@ namespace Fishermen.Controllers
             {
                 topTenHauls = topTenHauls.OrderByDescending(h => h.Caught);
             }
-            return Json(topTenHauls.Take(10).ToList());
+            return Json(topTenHauls.Take(rows).ToList());
         }
 
 
         //this method answers the question that Luke gave as an example, you give a list of systems and a month and it returns
-        //the 5 areas that historically have done the best in those systems during that month.
+        //the  areas that historically have done the best in those systems during that month.
         [HttpGet]
         [Route("api/BestPlaceToFishDuringMonth")]
         public IActionResult BestPlaceToFishDuringMonth(string[] listOfSystems, int month, int rows = 10)
@@ -84,9 +85,25 @@ namespace Fishermen.Controllers
                              join location in locations on haul.LocationId equals location.LocationId
                              where location.AreaNumber == areaNumber
                              group haul by haul.Month into haulGroup
-                             select new { Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(haulGroup.Key), FishCaught = haulGroup.Sum(h => h.Caught) });
+                             select new { Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(haulGroup.Key)//converts a month number to a month name
+                             , MonthNumber = haulGroup.Key
+                             ,FishCaught = haulGroup.Sum(h => h.Caught) });
 
-            return Json(bestMonth);
+            var bestMonthList = bestMonth.ToList();
+            if(bestMonthList.Count() != 12)
+            {
+                for (int i = 0; i < months.Count(); i++)
+                {
+                    if (bestMonthList.FindIndex(m => m.Month == months[i]) == -1)
+                    {
+                        bestMonthList.Add(new {Month = months[i], MonthNumber = i + 1, FishCaught = 0 });
+                    }
+
+                }
+            }
+
+            var result = bestMonthList.OrderBy(m => m.MonthNumber);
+            return Json(result);
         }
 
         //returns the total fish caught per year in all areas
@@ -169,6 +186,7 @@ namespace Fishermen.Controllers
             validMonths = validMonths.Take(validMonths.Count() - 1).ToArray();
 
             var validMonthObjectsAnon = new Object[12];
+            //add the month number to each month name.
             for(int i = 0; i < validMonths.Count(); i++)
             {
                 validMonthObjectsAnon[i] = new {MonthNumber = i + 1, MonthName = validMonths[i] };
@@ -193,11 +211,13 @@ namespace Fishermen.Controllers
 
             return Json(areaHistory.ToList());
         }
-        //this returns all the data we have with the only the most relevant columns included
+        //this query exists to give the user more control over the data. While the other queries answer specific predetermined questions
+        //this is closer to the functionality of an excel sheet where the user can apply a bunch of filters on many different columns to 
+        //answer whatever question they choose. I'm still pondering the best way to handle giving the user the ability to use group bys.
         [HttpGet]
         [Route("api/CustomQuery")]
         public IActionResult CustomQuery(int[] years, int[] months,
-            int[] areaNumbers, string[] areaNames, string[] regions, string[] systems,
+            int[] areaNumbers, string[] areaNames, string[] regions, string[] systems, string groupBy, bool average = true,
             int haulGreaterThan = -1, int haulLessThan = -1, int rows = 1000)
         {
             var hauls = fishHauls.TblHauls;
@@ -205,6 +225,7 @@ namespace Fishermen.Controllers
             var regionsTbl = fishHauls.TblRegions;
             var systemsTbl = fishHauls.TblSystems;
 
+            //this messy query joins all our tables to return all of the data we currently have
             var allData = from haul in hauls
                           join location in locations on haul.LocationId equals location.LocationId
                           join system in systemsTbl on location.SystemId equals system.SystemId
@@ -220,7 +241,9 @@ namespace Fishermen.Controllers
                               Year = haul.Year,
                               Month = haul.Month
                           };
-
+            //this applies our filters to the current data
+            //I'm debating making some sort of filter object and iterating over it
+            //but for now this works, even if it looks dumb.
             if (years.Count() > 0)
             {
                 allData = allData.Where(h => years.Contains(h.Year));
@@ -253,8 +276,109 @@ namespace Fishermen.Controllers
             {
                 allData = allData.Where(h => h.FishCaught < haulLessThan);
             }
+            //checks for groupBys and applies them if necessary
+            //lots of code needs to be duplicated because the grouped by data will be in slightly different
+            //based on where we grouped by. This prevents me from defining the groupedData variable outside of the inner if statements
+            if(groupBy != "None")
+            {
+                if(groupBy == "Date")
+                {
+                    var groupedData = allData.GroupBy(h => CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(h.Month) + " " + h.Year.ToString());
+                    groupedData.OrderBy(g => g.Key);
+                    if (average)
+                    {
+                        //I'm using vague property names here to make this data easier to work with on the front end
+                        //If the property names were different in each group by they would have to access the keys by index rather than
+                        //by name like has been done in the rest of the queries.
+                        var result = groupedData.Select(g => new {GroupKey= g.Key, FishCaught = g.Sum(h => h.FishCaught) / g.Count() });
+                        return Json(result);
+                    }
+                    else
+                    {
+                        var result = groupedData.Select(g => new {GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught)});
+                        return Json(result);
+                    }
+                }
+                else if (groupBy == "Month")
+                {
+                    var groupedData = allData.GroupBy(h => h.Month);
+                    groupedData.OrderBy(g => g.Key);
+                    if (average)
+                    {
+                        var result = groupedData.Select(g => new {GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) / g.Count() });
+                        return Json(result);
+                    }
+                    else
+                    {
+                        var result = groupedData.Select(g => new {GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) });
+                        return Json(result);
+                    }
+                }
+                else if (groupBy == "Year")
+                {
+                    var groupedData = allData.GroupBy(h => h.Year);
+                    
+                    if (average)
+                    {
+                        var result = groupedData.Select(g => new { GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) / g.Count() });
+                        result = result.OrderBy(g => g.GroupKey);
+                        return Json(result);
+                    }
+                    else
+                    {
+                        var result = groupedData.Select(g => new { GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) });
+                        result = result.OrderBy(g => g.GroupKey);
+                        return Json(result);
+                    }
+                }
+                else if (groupBy == "Area")
+                {
+                    var groupedData = allData.GroupBy(h => h.AreaNumber);
+                    groupedData.OrderBy(g => g.Key);
+                    if (average)
+                    {
+                        var result = groupedData.Select(g => new { GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) / g.Count() });
+                        return Json(result);
+                    }
+                    else
+                    {
+                        var result = groupedData.Select(g => new { GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) });
+                        return Json(result);
+                    }
+                }
+                else if (groupBy == "Region")
+                {
+                    var groupedData = allData.GroupBy(h => h.Region);
+                    groupedData.OrderBy(g => g.Key);
+                    if (average)
+                    {
+                        var result = groupedData.Select(g => new { GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) / g.Count() });
+                        return Json(result);
+                    }
+                    else
+                    {
+                        var result = groupedData.Select(g => new { GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) });
+                        return Json(result);
+                    }
+                }
+                else if (groupBy == "System")
+                {
+                    var groupedData = allData.GroupBy(h => h.System);
+                    groupedData.OrderBy(g => g.Key);
+                    if (average)
+                    {
+                        var result = groupedData.Select(g => new { GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) / g.Count() });
+                        return Json(result);
+                    }
+                    else
+                    {
+                        var result = groupedData.Select(g => new { GroupKey = g.Key, FishCaught = g.Sum(h => h.FishCaught) });
+                        return Json(result);
+                    }
+                }
+            }
 
-            return Json(allData.Take(rows));
+            return Json(allData.Take(rows));//now that we've done all the filtering we can apply our row count filter
         }
 
 
@@ -264,20 +388,5 @@ namespace Fishermen.Controllers
         {
             return HttpContext.Request.Path.Value + HttpContext.Request.QueryString.Value;
         }
-
-        [Serializable]
-        public class MonthObject
-        {
-            public int monthNumber;
-            public string monthName;
-
-            public MonthObject(int MonthNumber, string MonthName)
-            {
-                this.monthName = MonthName;
-                this.monthNumber = MonthNumber;
-            }
-        }
-
-
     }
 }
